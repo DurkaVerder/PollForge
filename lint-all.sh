f996ecc31b39c13fa5e360e1e1007dba9d49a947
FabrikantTimur <vrrrr227@gmail.com>
2025-04-17 02:52:54 +0300

    Update lint-all.sh

--- lint-all.sh
+++ lint-all.sh
@@ -8,49 +8,27 @@ NC='\033[0m'
 
 CONFIG_PATH=".golangci.yml"
 
-echo -e "${GREEN}Проверка кода...${NC}"
+echo -e "${NC}0_W_0 Проверка кода...${NC}"
 
 MODULE_DIRS=$(find . -mindepth 2 -name "go.mod" -exec dirname {} \;)
 
-count=0
 allModules=0
-for dir in $MODULE_DIRS; do
-  if [[ -d "$dir" ]]; then
-    allModules=$((allModules + 1))
-  fi
-done
 
 if [[ ! -f "$CONFIG_PATH" ]]; then
   echo -e "${RED}Конфиг $CONFIG_PATH не найден!${NC}"
   exit 1
 fi
 
-# Проходимся по каждому модулю
 for dir in $MODULE_DIRS; do
-  echo -e "${GREEN}▶Проверка модуля: $dir${NC}"
-  pushd "$dir" > /dev/null
-  if golangci-lint run --config="../$CONFIG_PATH" ./...; then
-    echo -e "${GREEN}✅ Проверка успешна: $dir${NC}"
-    count=$((count + 1))
-  else
-    echo -e "${RED}Проблемы найдены в: $dir${NC}"
+  if [[ -d "$dir" ]]; then
+    allModules=$((allModules + 1))
+    echo -e "${NC}Проверка модуля: $dir${NC}"
+    pushd "$dir" > /dev/null
+    golangci-lint run --config="../$CONFIG_PATH" ./...
+    echo -e "${GREEN}Проверка успешна: $dir${NC}"
+    echo ""
+    popd > /dev/null
   fi
-  echo -e ""
-  popd > /dev/null
 done
 
-if [[ $count -eq $allModules ]]; then
-  echo -e "${GREEN}Все модули прошли проверку!${NC}"
-else
-  echo -e "${RED}Проверка завершена с ошибками.${NC}"
-fi
-
-echo -e ""
-
-if [[ $count -eq $allModules ]]; then
-  echo -e "${GREEN}Успешно обработаны все модули.${NC}"
-  exit 1
-fi
-
-echo -e "${RED}Обработано $count из $allModules с ошибками.${NC}"
-
+echo -e "${GREEN}Все $allModules модуля прошли проверку без ошибок!${NC}"
