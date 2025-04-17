4f85b0cc1d8fbd6b5037b07f363106aaa38c8985
DurlaVerder <vrrrr227@gmail.com>
2025-04-17 21:03:38 +0300

    Split file

--- question/internal/service/service.go
+++ question/internal/service/service.go
@@ -2,16 +2,10 @@ package service
 
 import (
 	"errors"
-	"fmt"
 	"log"
-	"os"
 	"question/models"
 	"strconv"
-	"strings"
 	"sync"
-	"time"
-
-	"github.com/golang-jwt/jwt"
 )
 
 const (
@@ -179,62 +173,4 @@ func (s *Service) StartWorker(countWorkers int, wg *sync.WaitGroup) {
 		wg.Add(1)
 		go s.writeAnswerWorker(wg)
 	}
-}
-
-func parsedToken(tokenStr string) (*jwt.Token, error) {
-	parsedToken, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(os.Getenv("SECRET_KEY")), nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return parsedToken, nil
-}
-
-func validToken(token *jwt.Token) (bool, error) {
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		if !claims.VerifyExpiresAt(time.Now().Unix(), true) {
-			return false, errors.New("token is expired")
-		}
-		return true, nil
-	}
-	return false, errors.New("invalid token claims")
-}
-
-func getClaims(token *jwt.Token) (jwt.MapClaims, error) {
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		return claims, nil
-	}
-	return nil, errors.New("invalid token claims")
-}
-
-func GetParamFromJWT(tokenBearer, param string) (string, error) {
-	token := strings.TrimPrefix(tokenBearer, "Bearer ")
-
-	parsedToken, err := parsedToken(token)
-	if err != nil {
-		return "", err
-	}
-
-	valid, err := validToken(parsedToken)
-	if err != nil || !valid {
-		return "", err
-	}
-
-	claims, err := getClaims(parsedToken)
-	if err != nil {
-		return "", err
-	}
-
-	// Возможно будет ошибка с преобразованием
-
-	value, ok := claims[param].(string)
-	if !ok {
-		return "", fmt.Errorf("param %s not found in claims", param)
-	}
-
-	return value, nil
 }
