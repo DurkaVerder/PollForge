e97e959b71f38aa0c4763211c6251651dcd3b575
DurlaVerder <vrrrr227@gmail.com>
2025-04-17 13:17:36 +0300

    New ci

--- tidy-all.sh
+++ tidy-all.sh
@@ -1,17 +1,39 @@
-echo "Запуск go mod tidy для всех модулей..."
+#!/bin/bash
 
-find . -mindepth 2 -name "go.mod" -exec dirname {} \; | while read dir; do
-    echo "Модуль: $dir"
-    
-    pushd "$dir" > /dev/null
+set -euo pipefail
 
-    go mod tidy
-    if ! git diff --quiet go.mod go.sum; then
-        echo "❌ go.mod/go.sum изменены в $dir. Запусти 'go mod tidy'."
-        git diff go.mod go.sum
-        exit 1
-    fi
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 
-    popd > /dev/null
+echo -e "${NC}^-^ Запуск go mod tidy для всех модулей...${NC}"
 
+MODULE_DIRS=$(find . -mindepth 2 -name "go.mod" -exec dirname {} \;)
+
+any_failed=0
+
+for dir in $MODULE_DIRS; do
+  echo -e "${NC}Модуль: $dir${NC}"
+  pushd "$dir" > /dev/null
+
+
+  OUTPUT=$(go mod tidy -v 2>&1)
+
+  # Если есть сообщения о скачивании зависимостей, то фейлим
+  if echo "$OUTPUT" | grep -qE "go: downloading|go: finding module for package";  then
+    echo -e "${RED}Ошибка: go mod tidy попытался скачать зависимости в $dir:${NC}"
+    echo "$OUTPUT"
+    any_failed=1
+  else
+    echo -e "${GREEN}Успешно: $dir${NC}"
+  fi
+
+  popd > /dev/null
 done
+
+if [[ "$any_failed" == "1" ]]; then
+  echo -e "${RED}Некоторые модули не прошли go mod tidy${NC}"
+  exit 1
+else
+  echo -e "${GREEN}Все модули успешно обработаны go mod tidy${NC}"
+fi
