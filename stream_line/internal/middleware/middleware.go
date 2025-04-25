4f27ee99a793422508bc71e2850a116a913a8de8
DurlaVerder <vrrrr227@gmail.com>
2025-04-25 12:06:52 +0300

    Added iddleware for check userID

--- stream_line/internal/middleware/middleware.go
+++ stream_line/internal/middleware/middleware.go
@@ -1,9 +1,29 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"stream_line/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
 
 func AuthMiddleware(c gin.Context) {
-	// Check exists userID in the jwt
+
+	jwt := c.GetHeader("Authorization")
+	if jwt == "" {
+		c.Set("userID", "-1") // No userID in the jwt, set to -1
+		c.Next()
+	}
+
+	userID, err := service.GetParamFromJWT(jwt, "id")
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Unauthorized",
+		})
+		c.Abort()
+	}
+
+	c.Set("userID", userID)
 
 	c.Next()
 }
