aafc951ea8e14782f182fa7d4e24ce3a8dc0dfa4
DurlaVerder <vrrrr227@gmail.com>
2025-04-26 14:13:20 +0300

    Added server and middleware logger

--- stream_line/internal/middleware/middleware.go
+++ stream_line/internal/middleware/middleware.go
@@ -1,29 +1,42 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 	"stream_line/internal/service"
 
 	"github.com/gin-gonic/gin"
 )
 
-func AuthMiddleware(c *gin.Context) {
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		jwt := c.GetHeader("Authorization")
+		if jwt == "" {
+			c.Set("userID", "-1") // No userID in the jwt, set to -1
+			c.Next()
+		}
 
-	jwt := c.GetHeader("Authorization")
-	if jwt == "" {
-		c.Set("userID", "-1") // No userID in the jwt, set to -1
+		userID, err := service.GetParamFromJWT(jwt, "id")
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Unauthorized",
+			})
+			c.Abort()
+		}
+
+		c.Set("userID", userID)
+
 		c.Next()
 	}
+}
 
-	userID, err := service.GetParamFromJWT(jwt, "id")
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"error": "Unauthorized",
-		})
-		c.Abort()
+func LoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		logger := log.New(c.Writer, "[Middleware]", log.LstdFlags|log.Lshortfile)
+		logger.Printf("Request: %s %s", c.Request.Method, c.Request.URL.Path)
+
+		c.Next()
+
+		logger.Printf("Response: %d", c.Writer.Status())
 	}
-
-	c.Set("userID", userID)
-
-	c.Next()
 }
---
+++ stream_line/internal/server/server.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"stream_line/internal/handlers"
+	"stream_line/internal/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	handlers *handlers.StreamLineHandler
+
+	engine *gin.Engine
+}
+
+func NewServer(handlers *handlers.StreamLineHandler, engine *gin.Engine) *Server {
+	return &Server{
+		handlers: handlers,
+		engine:   engine,
+	}
+}
+
+func (s *Server) initRoutes() {
+
+	streamLine := s.engine.Group("/streamline")
+	streamLine.Use(middleware.AuthMiddleware())
+	{
+		streamLine.GET("/streamline", s.handlers.GetStreamLine)
+	}
+
+}
+
+func (s *Server) Start(port string) {
+
+	s.initRoutes()
+
+	if err := s.engine.Run(port); err != nil {
+		panic(err)
+	}
+}
