09b2b26b2fc2b5812839cf7b54ae80dbb42130e6
ApostolPac <pasha2005plovec@yahoo.com>
2025-05-03 19:26:47 +0300

    a

--- forms/internal/service/service.go
+++ forms/internal/service/service.go
@@ -16,7 +16,7 @@ func GetToken(auth string) (*jwt.Token, error) {
 	tokenStr := strings.TrimPrefix(auth, "Bearer")
 	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
 		if t.Method != jwt.SigningMethodHS256 {
-			return nil, fmt.Errorf("Неподходящий метод подписи")
+			return nil, fmt.Errorf("неподходящий метод подписи")
 		}
 		return []byte(os.Getenv("JWT_SECRET")), nil
 	})
