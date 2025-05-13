2118087d01d44467cc68927e770734a7c832fee5
DurlaVerder <vrrrr227@gmail.com>
2025-05-13 19:33:17 +0300

    Rewrite streamlines

--- services/stream_line/internal/models/models.go
+++ services/stream_line/internal/models/models.go
@@ -2,26 +2,32 @@ package models
 
 import "time"
 
-type Form struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Likes       Like   `json:"likes"`
-	CreatedAt   string `json:"created_at"`
-	ExpiresAt   string `json:"expires_at"`
+type StreamLineResponse struct {
+	Polls []Polls `json:"polls"`
 }
 
-type FormResponse struct {
-	Forms []Form `json:"forms"`
+type Polls struct {
+	ID          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Likes       Like       `json:"likes"`
+	CountVotes  int        `json:"count_votes"`
+	Questions   []Question `json:"questions"`
+	CreatedAt   string     `json:"created_at"`
+	ExpiresAt   string     `json:"expires_at"`
 }
 
-type FormFromDB struct {
-	ID          int
-	Title       string
-	Description string
-	Like        LikeFromDB
-	CreatedAt   time.Time
-	ExpiresAt   time.Time
+type Question struct {
+	ID      int      `json:"id"`
+	Title   string   `json:"title"`
+	Answers []Answer `json:"answers"`
+}
+
+type Answer struct {
+	ID         int     `json:"id"`
+	Title      string  `json:"title"`
+	Percent    float64 `json:"percent"`
+	IsSelected bool    `json:"is_selected"`
 }
 
 type Like struct {
@@ -32,4 +38,30 @@ type Like struct {
 type LikeFromDB struct {
 	Count   int
 	IsLiked bool
+}
+type FormFromDB struct {
+	ID          int
+	Title       string
+	Description string
+	Like        LikeFromDB
+	CountVotes  int
+	CreatedAt   time.Time
+	ExpiresAt   time.Time
+}
+
+type QuestionFromDB struct {
+	ID          int
+	Title       string
+	FormID      int
+	NumberOrder int
+}
+
+type AnswerFromDB struct {
+	ID          int
+	Title       string
+	QuestionID  int
+	NumberOrder int
+	CountVotes  int
+	IsSelected  bool
+	Percent     float64
 }
